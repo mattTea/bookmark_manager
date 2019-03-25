9098cabb219c59466411bf0b7712c5c39ccc9f6b
Arthur Ashman <arthurashman@gmail.com>
2019-03-25 15:45:09 +0000

    adds a button to homepage to view bookmarks

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,14 @@
 require_relative '../../app.rb'
 
 feature 'Homepage' do
-  scenario 'user welcomed to the app' do
+  scenario 'welcomes user to the app' do
     visit ('/')
     expect(page).to have_content "Welcome to Bookmark Manager!"
   end
+
+  scenario 'has a button to view bookmarks' do
+    visit ('/')
+    expect(page).to have_selector 'button'
+  end
 end
+
