986081964a81b7c6414a4986196b87fc402c6326
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-26 16:50:29 +0000

    Refactors previous commit

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,17 +2,9 @@ require 'pg'
 
 class Bookmark
   def self.all
-    # ["www.google.com", "www.facebook.com"]
-
-    conn = PG.connect( dbname: 'bookmark_manager' )
-    result = conn.exec( "SELECT * FROM bookmarks" )
-    # return result.getvalue(0,1)
-    bookmarks_list = result.values
-
-    list = []
-    bookmarks_list.each do |id, url|
-      list << url
-    end
+    connection = PG.connect( dbname: 'bookmark_manager' )
+    result = connection.exec( "SELECT * FROM bookmarks;" )
+    list = result.map { |bookmark| bookmark['url'] }
     list.join("\n")
   end
 end
